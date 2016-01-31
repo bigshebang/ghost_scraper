0faeff9c07a72caf813a945824e1c18973ab06c0
bigshebang <ljm3103@rit.edu>
2016-01-31 13:25:31 -0500

    add removeProxy function

--- scrape.py
+++ scrape.py
@@ -4,10 +4,12 @@ import time
 from multiprocessing import Process, Lock
 import platform
 import subprocess as sub
+import shutil
 
 urls = []
 permFilename = "permutations"
 proxyFilename = "proxies"
+badProxyfilename = ".bad_proxies"
 logFilename = ".ghost_scraper.log"
 safeTries = 7 #safe number of URLs to try at once. not changeable
 current = 0
@@ -31,7 +33,7 @@ def scrape(urls, p):
 
 		#if we get blocked return false
 		if ret == 420:
-			#removeProxy() #get rid of proxy from proxylist
+			removeProxy() #get rid of proxy from proxylist
 			return false
 		else: #successfully tried to get the URL
 			urls[i]["status"] = 1
@@ -42,8 +44,12 @@ def scrape(urls, p):
 #return status code
 def tryURL(url, p):
 	#TODO: add a try/catch all around the GET request
-	r = requests.get("https://ghostbin.com/paste/" + url + "/raw",
-		proxies=p)
+	try:
+		r = requests.get("https://ghostbin.com/paste/" + url + "/raw",
+			proxies=p)
+	except: #error so proxy is probably bad
+		return 420
+
 	print r.status_code,
 	if r.status_code != 404:
 		print r.text
@@ -65,7 +71,7 @@ def getURLs(f, urls=[]):
 				break
 
 			#get URL and put in list. if end of file we're done
-			line = f.readline()
+			line = f.readline().rstrip()
 			if line == "":
 				if i == 0: #this means first thing we read is empty file
 					#check if there are any URLs leftover from last time
@@ -88,20 +94,19 @@ def getURLs(f, urls=[]):
 #return a proxy as a string from the file
 def getProxy(f, proxies=[], index=0):
 	#get a new proxy
-	line = f.readline()
+	line = f.readline().rstrip()
 
 	#if end of file, start from beginning of file and try again
 	if line == "":
 		f.flush()
-		line = f.readline()
+		line = f.readline().rstrip()
 
 		if line == "": #if file is empty, no more proxies left
 			#code
 
 	counter = 0	
-	while line not in proxies:
-		proxies[index] = line
-		line = f.readline()
+	while line in proxies and not proxyCheck(line): #if proxy in use already
+		line = f.readline().rstrip()
 
 		#this means we ran very low on proxies. sleep before trying again
 		if line == "":
@@ -112,6 +117,57 @@ def getProxy(f, proxies=[], index=0):
 			sleep 5
 
 		counter += 1
+
+	proxies[index] = line
+
+#will remove a given proxy from the proxy list file
+def removeProxy(f, proxy):
+	#add bad proxy to bad proxies list
+	with open(badProxyFilename, "a") as badProxyFile:
+		badProxyFile.write(proxy + "\n")
+
+	#get current position in file
+	current = f.tell() - len(proxy) + 1
+	f.flush() #go to beginning of file to read in all proxies
+	lines = []
+	line = f.readline().rstrip()
+
+	#read in lines until we hit the end of the file
+	while line != "":
+		if line != proxy: #if it isn't the bad proxy, remember it
+			lines.append(line)
+
+		line = f.readline().rstrip()
+	
+	#move file to backup
+	f.close()
+	shutil.move(proxyFilename, ".old_" + proxyFilename)
+
+	#write new file without bad proxy
+	with open(proxyFilename, "w") as outfile:
+		for p in lines:
+			outfile.write(p + "\n")
+	
+	#we can get rid of backup proxy list cuz we made the other one now
+	shutil.rmtree(".old_" + proxyFilename)
+
+	#go back to where we were in the file
+	f = open(proxyFilename, "r")
+	f.seek(current, 0)
+
+#check if proxy works and can connect to the internet with it
+#return true if it's good, false if it's not
+def proxyCheck(proxy):
+	proxies = {
+		"https" : proxy
+	}
+
+	try:
+		r = requests.get("https://www.google.com", proxies=proxies)
+	except:
+		return false
+	
+	return true
 
 #count num lines in a given file
 def getLines(filename):
@@ -165,7 +221,7 @@ def main():
 	while not done:
 		for num in range(0, numProc):
 			if not p[num].is_alive(): #check if process is finished
-				if sleep: #sleep if too many more processes than proxies
+				if sleep: #sleep if too many processes compared to proxies
 					time.sleep(proxyBuffer)
 
 				#get new URLs and proxy
