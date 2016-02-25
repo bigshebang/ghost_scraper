4329a3ae13a4c2340ebe2c7ec251240383a97320
Luke <ljm3103@rit.edu>
2016-02-24 19:37:16 -0500

    parse json, split up port and ip and add to list

--- addproxy.py
+++ addproxy.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import requests
+import requests, json
 
 happyTokenFilename = "happy_proxy"
 
@@ -8,13 +8,33 @@ def run():
 	token = ""
 	with open(happyTokenFilename) as f:
 		token = f.readline().rstrip()
+
+	#make request for proxies
+	r = requests.get("https://happy-proxy.com/fresh_proxies?key=" + token)
+
+	#parse received json if successful request
+	if r.status_code == 200 and r.text != "":
+		result = json.loads(r.text)
+	else:
+		return
+
+	#print type(result)
+	#print result
+	proxies = []
+	#parse data to get a list of lists
+	#each item in the bigger list will be a list containing the IP and port
+	#of the proxy
+	for entry in result:
+		temp = entry["ip_port"]
+		vals = temp.split(":")
+		proxies.append(vals)
 	
-	#code
-	print token
+	for p in proxies:
+		print p[0] + ":" + p[1]
 
-#def main():
-#	run()
+def main():
+	run()
 
-#if __name__ == "__main__":
-#	main()
+if __name__ == "__main__":
+	main()
 
